533dd518e89f678468089cb06d812139cb60d380
Francois Visconte <f.visconte@gmail.com>
2014-05-18 12:21:25 +0200

    Add some tests

--- dssh/conf_test.go
+++ dssh/conf_test.go
@@ -2,6 +2,8 @@ package dssh
 
 import (
   "github.com/bmizerany/assert"
+  "io/ioutil"
+  "syscall"
   "testing"
 )
 
@@ -22,11 +24,18 @@ group2:
     - h2
 `
 
+func unsetConfig() {
+  _registry = nil
+}
+
 func TestLoadConfig(t *testing.T) {
+  defer unsetConfig()
   LoadConfig([]byte(sample_config))
 }
 
 func TestGetExistingGroup(t *testing.T) {
+  LoadConfig([]byte(sample_config))
+  defer unsetConfig()
   g, err := GetGroup("group1")
   assert.NotEqual(t, g, nil)
   assert.Equal(t, err, nil)
@@ -36,7 +45,36 @@ func TestGetExistingGroup(t *testing.T) {
   t.Logf("Loaded :%#v", g)
 }
 
+func TestLoadConfigFromFile(t *testing.T) {
+  f, err := ioutil.TempFile("", "config")
+  defer syscall.Unlink(f.Name())
+  defer unsetConfig()
+  if err != nil {
+    panic(err)
+  }
+  ioutil.WriteFile(f.Name(), []byte(sample_config), 0644)
+  LoadConfigFromFile(f.Name())
+  assert.NotEqual(t, _registry, nil)
+}
+
+func TestLoadConfigFromNonExistingFile(t *testing.T) {
+  err := LoadConfigFromFile("non-existent-file")
+  assert.NotEqual(t, err, nil)
+}
+
+func TestLoadInvalidConfig(t *testing.T) {
+  invalidConfig := `
+{ 
+  "asdaidsasd
+asdasdasda--d-adasd
+  `
+  err := LoadConfig([]byte(invalidConfig))
+  assert.NotEqual(t, err, nil)
+}
+
 func TestGetNonExistingGroup(t *testing.T) {
+  LoadConfig([]byte(sample_config))
+  defer unsetConfig()
   _, err := GetGroup("Do-not-exists")
   assert.NotEqual(t, err, nil)
 }
